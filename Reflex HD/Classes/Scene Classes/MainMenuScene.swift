129b98b25ca0a19060c760889ca5dbb628bce977
SocietySF <societysfsup@gmail.com>
2018-04-08 20:05:01 +0200

    Impostazione base

---
+++ Reflex HD/Classes/Scene Classes/MainMenuScene.swift
@@ -0,0 +1,17 @@
+//
+//  MainMenuScene.swift
+//  Reflex HD
+//
+//  Created by Simone Leoni on 08/04/18.
+//  Copyright © 2018 Society SF. All rights reserved.
+//
+
+import SpriteKit
+
+class MainMenuScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        <#code#>
+    }
+
+}
