0bd7dd1325f279ecd7a6b4a072d3c5fb58d38682
SocietySF <societysfsup@gmail.com>
2018-04-19 20:17:13 +0200

    Nuova Classe e Scena per Settings. Simone Leoni

---
+++ Reflex HD/Classes/Scene Classes/SettingsScene.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsScene.swift
+//  Reflex HD
+//
+//  Created by Simone Leoni on 19/04/18.
+//  Copyright © 2018 Society SF. All rights reserved.
+//
+
+import SpriteKit
+
+class SettingsScene: SKScene {
+
+}
