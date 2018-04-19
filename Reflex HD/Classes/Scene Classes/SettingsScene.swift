ed151851bf7bfe0952e0f0b95d32d6294a2f6700
SocietySF <societysfsup@gmail.com>
2018-04-19 20:29:52 +0200

    codice settings Francesco Castelli

--- Reflex HD/Classes/Scene Classes/SettingsScene.swift
+++ Reflex HD/Classes/Scene Classes/SettingsScene.swift
@@ -9,5 +9,24 @@
 import SpriteKit
 
 class SettingsScene: SKScene {
-
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        for touch in touches {
+            let location = touch.location(in: self);
+            
+            if atPoint(location).name == "Settings" {
+                if let scene = SettingsScene(fileNamed: "Settings") {
+                    // Set the scale mode to scale to fit the window
+                    scene.scaleMode = .aspectFill
+                    
+                    // Present the scene
+                    view!.presentScene(scene)
+                }
+            }
+            
+            
+        }
+        
+    }
 }
