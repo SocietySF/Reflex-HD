7954a39bf603a44dfb6970f3ba49cf7183f4eb65
SocietySF <societysfsup@gmail.com>
2018-04-22 01:19:19 +0200

    Aggiunta dei tasti indietro provvisori in tutte le schermate e FPS true. Simone Leoni

--- Reflex HD/Classes/Scene Classes/SettingsScene.swift
+++ Reflex HD/Classes/Scene Classes/SettingsScene.swift
@@ -10,5 +10,26 @@ import SpriteKit
 
 class SettingsScene: SKScene {
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        for touch in touches {
+            let location = touch.location(in: self);
+            
+            if atPoint(location).name == "Back" {
+                if let scene = MainMenuScene(fileNamed: "MainMenu") {
+                    // Set the scale mode to scale to fit the window
+                    scene.scaleMode = .aspectFill
+                    
+                    // Present the scene
+                    view!.presentScene(scene)
+                    
+                }
+                
+            }
+            
+        }
+        
+    }
+        
 }
    
--- Reflex HD/Classes/Scene Classes/ShopScene.swift
+++ Reflex HD/Classes/Scene Classes/ShopScene.swift
@@ -10,4 +10,25 @@ import SpriteKit
 
 class ShopScene: SKScene {
 
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        for touch in touches {
+            let location = touch.location(in: self);
+            
+            if atPoint(location).name == "Back" {
+                if let scene = MainMenuScene(fileNamed: "MainMenu") {
+                    // Set the scale mode to scale to fit the window
+                    scene.scaleMode = .aspectFill
+                    
+                    // Present the scene
+                    view!.presentScene(scene)
+                    
+                }
+                
+            }
+            
+        }
+        
+    }
+    
 }
